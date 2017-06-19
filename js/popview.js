6c87b62d5630ecee9a5789ffa4dd16378d03311a
fztfztfztfzt <fztfztfztfzt@gmail.com>
2017-06-19 17:47:10 +0800

    修改界面

--- js/popview.js
+++ js/popview.js
@@ -10,8 +10,47 @@
         var padding1 = 20;
         var width_svg = width-2*padding0;
         var height_svg = (height-2*padding0)/3;
-        var color0 = ['rgb(241,125,48)',"#9467bd"];
-        var color = ['rgb(241,125,48)','rgb(36,120,180)',"#8c564b","#9467bd",'rgb(215,63,42)','rgb(76,160,46)'];
+        var color0 = ['#FFCC99',"#CCCCFF"];
+        var color = ['#FFCC99','#66CCFF'];
+        var color2 = ['#FF99CC','#FFFF99','#99CC66'];
+        //svg0
+        var svg0 = d3.select('#p4_pop').append('svg').attr('width',width_svg).attr('height',35);
+        svg0.append('circle')
+            .attr('cx',30)
+            .attr('cy',20)
+            .attr('r',10)
+            .attr('fill',color0[0]);
+        svg0.append('text')
+            .attr('x',50)
+            .attr('y',25)
+            .text('未成年人')
+            .attr("font-family", "Courier New")
+            .attr("fill", "white")
+            .attr("font-size", "20px");
+        svg0.append('circle')
+            .attr('cx',160)
+            .attr('cy',20)
+            .attr('r',10)
+            .attr('fill',color0[1]);
+        svg0.append('text')
+            .attr('x',180)
+            .attr('y',25)
+            .text('成年人')
+            .attr("font-family", "Courier New")
+            .attr("fill", "white")
+            .attr("font-size", "20px");
+        svg0.append('circle')
+            .attr('cx',275)
+            .attr('cy',20)
+            .attr('r',10)
+            .attr('fill',color[1]);
+        svg0.append('text')
+            .attr('x',295)
+            .attr('y',25)
+            .text('流动人口')
+            .attr("font-family", "Courier New")
+            .attr("fill", "white")
+            .attr("font-size", "20px");
         //svg1用于显示饼图
         var svg1 = d3.select('#p4_pop').append('svg').attr('width',width_svg).attr('height',height_svg);
         var p4_1 = svg1.append('g').attr('transform', "translate(" + width_svg/4 + ',' + height_svg/2 + ')');
@@ -37,47 +76,45 @@
                 return "translate(" + arc.centroid(d) + ")";
             })
             .attr("text-anchor","middle")
+            .attr("fill", "white")
             .text(function(d){
                 return d.data;
             })
             .attr("class","p4_1t");
             
-        svg1.append('circle')
-            .attr('cx',width_svg/2+40)
-            .attr('cy',50)
-            .attr('r',10)
-            .attr('fill',color0[0]);
-        svg1.append('text')
-            .attr('x',width_svg/2+60)
-            .attr('y',55)
-            .text('TODO1')
-            .attr("font-family", "Courier New")
-            .attr("font-size", "20px");
-        svg1.append('circle')
-            .attr('cx',width_svg/2+40)
-            .attr('cy',90)
-            .attr('r',10)
-            .attr('fill',color0[1]);
-        svg1.append('text')
-            .attr('x',width_svg/2+60)
-            .attr('y',95)
-            .text('TODO2')
-            .attr("font-family", "Courier New")
-            .attr("font-size", "20px");
-        svg1.append('circle')
-            .attr('cx',width_svg/2+40)
-            .attr('cy',130)
-            .attr('r',10)
-            .attr('fill',color[1]);
-        svg1.append('text')
-            .attr('x',width_svg/2+60)
-            .attr('y',135)
-            .text('TODO3')
-            .attr("font-family", "Courier New")
-            .attr("font-size", "20px");
+        var p4_0 = svg1.append('g').attr('transform', "translate(" + width_svg*3/4 + ',' + height_svg/2 + ')');
+        var dataset = [1,2,3];
+        var pie = d3.layout.pie().sort(null).value(function(d){return d;})(dataset);
+        var radius = (d3.min([width_svg,height_svg])-padding1)/2;
+        var arc = d3.svg.arc().innerRadius(0).outerRadius(radius);
+        var arcs = p4_0.selectAll('g')
+            .data(pie)
+            .enter()
+            .append('g')
+            .attr('class', "p4_0g");
+        arcs.append('path')
+            .attr('fill', function(d, i){
+                return color2[i];
+            })
+            .attr('d', function(d){
+                return arc(d);
+            })
+            .attr('class', "p4_0");
+        arcs.append("text")
+            .attr("transform",function(d){
+                return "translate(" + arc.centroid(d) + ")";
+            })
+            .attr("text-anchor","middle")
+            .attr("fill", "white")
+            .text(function(d){
+                return d.data;
+            })
+            .attr("class","p4_0t");
+            
+        
 
         //svg2用于显示上网高峰折线图
-        var svg2 = d3.select('#p4_pop').append('svg').attr('width',width_svg).attr('height',height_svg);
+        var svg2 = d3.select('#p4_pop').append('svg').attr('width',width_svg).attr('height',height_svg+10);
         temp = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
         var xScale2 = d3.scale.linear().domain([0,23]).range([padding1*3,width_svg-padding1]);
         var yScale2 = d3.scale.linear().domain([0,100]).range([height_svg-padding1,padding1]);
@@ -149,6 +186,7 @@
             .attr('y',height_svg-padding0)
             .attr("font-family", "Courier New")
             .attr("font-size", "20px")
+            .attr("fill", "white")
             .attr("text-anchor", "middle")
             .attr('class','p4_3_0');
         svg3.selectAll('.p4_3_1')
@@ -198,21 +236,92 @@
             
             
         function update(id){
+            //top3TODO
+            $.ajax({
+                type: "POST",
+                url: "http://182.254.134.126:9494/get_province_distributed_by_barid",
+                dataType: "json",
+                contentType: "application/json;charset=utf-8",
+                data: JSON.stringify({id:333}),
+                success: function(data) {
+                    console.log(data.res);
+                    if(data.status==0){return;}
+                },
+                error: function(message) {
+                    console.log("fail");
+                }
+            });
+            //饼图TODO
+            $.ajax({
+                type: "POST",
+                url: "http://182.254.134.126:9494/get_province_distributed_by_barid",
+                dataType: "json",
+                contentType: "application/json;charset=utf-8",
+                data: JSON.stringify({id:333}),
+                success: function(data) {
+                    console.log(data.res);
+                    if(data.status==0){return;}
+                },
+                error: function(message) {
+                    console.log("fail");
+                }
+            });
+            //折线图TODO
+            $.ajax({
+                type: "POST",
+                url: "http://182.254.134.126:9494/get_province_distributed_by_barid",
+                dataType: "json",
+                contentType: "application/json;charset=utf-8",
+                data: JSON.stringify({id:333}),
+                success: function(data) {
+                    console.log(data.res);
+                    if(data.status==0){return;}
+                },
+                error: function(message) {
+                    console.log("fail");
+                }
+            });
+            //柱状图TODO
+            $.ajax({
+                type: "POST",
+                url: "http://182.254.134.126:9494/get_province_distributed_by_barid",
+                dataType: "json",
+                contentType: "application/json;charset=utf-8",
+                data: JSON.stringify({id:333}),
+                success: function(data) {
+                    console.log(data.res);
+                    if(data.status==0){return;}
+                },
+                error: function(message) {
+                    console.log("fail");
+                }
+            }); 
             d3.json("test.json",function(error,data){
-                d3.select("#p4_0").text("TOP3:"+data[0][0]+','+data[0][1]+','+data[0][2]);
-                
+                var pie = d3.layout.pie().sort(null).value(function(d){return d;})(data[0]);
+                d3.selectAll(".p4_0")
+                    .data(pie)
+                    .attr('d', function(d){
+                        return arc(d);
+                    })
+                d3.selectAll(".p4_0t")
+                    .data(pie)
+                    .attr("transform",function(d){
+                        return "translate(" + arc.centroid(d) + ")";
+                    })
+                    .text(function(d){
+                        return d.data;
+                    });
                 var pie = d3.layout.pie().sort(null).value(function(d){return d;})(data[1]);
                 d3.selectAll(".p4_1")
                     .data(pie)
                     .attr('d', function(d){
                         return arc(d);
-                    })
+                    });
                 d3.selectAll(".p4_1t")
                     .data(pie)
                     .attr("transform",function(d){
                         return "translate(" + arc.centroid(d) + ")";
                     })
-                    .attr("text-anchor","middle")
                     .text(function(d){
                         return d.data;
                     });
